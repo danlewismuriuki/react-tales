2f9bd13c8868a4c12e36b2f358d9a640dea0f814
danlewismuriuki <danlewismuriuki2@gmail.com>
2025-04-12 11:16:31 +0300

    feat: add dynamic greeting component based on current time

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-export function Time () {
+export function Time() {
+  const date = new Date();
+  const hour  = date.getHours();
 
-  const currentHour = new Date().getHours();
-
-  if (currentHour < 12)
+  if (hour >= 6  && hour < 12) {
     return <h1>Good Morning</h1>
-    else
-    return <h1>Good Evening</h1>
+  }
+  else if (hour >= 12 && hour < 17) {
+    return <h1>Good Afternoon</h1>
+  }
+  else {
+  return <h1>Good Evening</h1>
+}
 }
 
 export function App () {
   return (
-    <Time/>
+    <Time />
   )
 }
 
