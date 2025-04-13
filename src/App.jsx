8ca31e7407d556a662d8d7f30d466630f9ee2b6a
danlewismuriuki <danlewismuriuki2@gmail.com>
2025-04-13 23:26:05 +0300

    feat: add ActionCard component with function passed as prop for click handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-export function ClickCard({
-  color="Red",
-  text="Surprised again",
-  onClickAction = () => console.log("Default click handler")
-}) {
+export function ActionCard ({message="Defaul Messsage", actionHandler}) {
   const style = {
-    color: color
+    backgroundColor: "lightblue"
   }
 
   return (
-    <div style={style} onClick={() => onClickAction(text)}>
-      <h1>{text}</h1>
+    <div style = {style} onClick={() => actionHandler(message)}> 
+    {message}
     </div>
   )
 }
 
-
 export function App() {
   return (
     <>
-    <ClickCard text="Surprise!" color="purple" onClickAction = {(t) => console.log(`You clicked: ${t}`)}/>
-    <ClickCard onClickAction = {(t) => console.log(`You clicked: ${t}`)}/>
-    <ClickCard text="Surprise!" color="purple"/>
+    <ActionCard message="This is an action messsage" actionHandler={(msg) => console.log(`The card says: ${msg}`)}/>
     </>
   )
 }
-
 export default App;
