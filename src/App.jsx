1b8072e9987f8bf1c112d367942f74c8537b6afd
danlewismuriuki <danlewismuriuki2@gmail.com>
2025-04-13 22:22:06 +0300

    feat: add default onClickAction handler to ClickCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-export function WelcomeBox({ message = "Welcome!", handleWelcome }) {
+export function ClickCard({
+  color="Red",
+  text="Surprised again",
+  onClickAction = () => console.log("Default click handler")
+}) {
+  const style = {
+    color: color
+  }
+
   return (
-    <div onClick={ () => handleWelcome(message)}>
-      {message}
+    <div style={style} onClick={() => onClickAction(text)}>
+      <h1>{text}</h1>
     </div>
-  );
+  )
 }
+
 
 export function App() {
   return (
-    <WelcomeBox
-      message="Click to Welcome"
-      handleWelcome={(msg) => alert(`Message: ${msg}`)}
-    />
-  );
+    <>
+    <ClickCard text="Surprise!" color="purple" onClickAction = {(t) => console.log(`You clicked: ${t}`)}/>
+    <ClickCard onClickAction = {(t) => console.log(`You clicked: ${t}`)}/>
+    <ClickCard text="Surprise!" color="purple"/>
+    </>
+  )
 }
-
 
 export default App;
