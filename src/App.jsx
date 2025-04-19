afe249fde588e2fdd3d3f00c44d6ebc3f4db6e14
danlewismuriuki <danlewismuriuki2@gmail.com>
2025-04-19 12:43:01 +0300

    feat: add reset message feedback and prevent decrement below zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 
-export function Toggle() {
-  const [toggle, setToggle]=useState(true)
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
+export function Counter() {
+  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("")
+
+  function Increment () {
+    setCount(count + 1)
+    setMessage("")
+  }
+
+  function Decrement () {
+    if (count > 0) {
+      setCount(count - 1 )
+      setMessage("")
+    }
+  }
+
+  function Reset () {
+    if (count == 0) {
+      setMessage("count is already at 0!")
+    }
+    setCount(0)
+    setMessage("Count has been reset")
+  }
 
   return (
-    <div>
-      <p>The switch is {toggle ? 'ON': 'OFF'}</p>
-      <button onClick={handleToggle}>
-        Toggle
-      </button>
-    </div>
+  <div>
+    <p>Count: {count}</p>
+    <button onClick={Increment}>Increase</button>
+    <button onClick={Decrement}>Decrease</button>
+    <button onClick={Reset}>Reset</button>
+    {message && <p style={{color: "red"}}>{message}</p>}
+  </div>
   )
 }
 
-export default Toggle;
+export default Counter;
