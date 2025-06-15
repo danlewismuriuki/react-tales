a8470b371c958c90f8e977530c26c4e7aef90532
danlewismuriuki <danlewismuriuki2@gmail.com>
2025-06-15 19:50:30 +0300

    refactor: move routing logic to router/index.js for better scalability

---
+++ reactRoutingV2/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Profile from './page/Profile'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
+
